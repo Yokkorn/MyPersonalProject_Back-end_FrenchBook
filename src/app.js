34e52890675a4b9b508f80e495308edf3fed78d1
Yokkorn <suratipyok@gmail.com>
2023-03-11 22:56:47 +0700

    config essentail middleware

--- src/app.js
+++ src/app.js
@@ -1,2 +1,32 @@
-// const { sequelize } = require("./models");
-// sequelize.sync({ force: true });
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errordMiddleware = require("./middlewares/error");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 10000,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errordMiddleware);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  console.log(chalk.bgMagenta(`server is running on port: ${port}`))
+);
