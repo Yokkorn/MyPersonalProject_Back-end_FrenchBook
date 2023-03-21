e0cc79099623effcac95aa194178219441bbd050
Yokkorn <suratipyok@gmail.com>
2023-03-22 00:29:56 +0700

    feature login

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -4,6 +4,7 @@ const {
 } = require("../validators/auth-validator");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 const createError = require("../utils/create-error");
 
@@ -33,7 +34,29 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    // const value = validateLogin(req.body);
+    const value = validateLogin(req.body);
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email: value.emailOrMobile },
+          { mobile: value.emailOrMobile },
+        ],
+      },
+    });
+    if (!user) {
+      createError("invalid email or mobile or password", 400);
+    }
+
+    const isCorrect = await bcrypt.compare(value.password, user.password);
+    if (!isCorrect) {
+      createError("invalid email or mobile or password", 400);
+    }
+
+    const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({ accessToken });
   } catch (err) {
     next(err);
   }
